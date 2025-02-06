aca88327710347c7e29bf0a6bfc96fed50bea912
fermin_isaias <ferminquite01@gmail.com>
2025-02-05 18:47:34 -0600

    Agregar archivos de proyecto.

---
+++ 20LIDTS-FerminPalacios-04py/_20LIDTS_FerminPalacios_04py.py
@@ -0,0 +1,17 @@
+maxf=3
+maxc=3
+
+a=[[0.0]*maxc for _ in range(maxf)]
+
+#leer array
+for f in range(maxf):
+    for c in range(maxc):
+        print("ingrese un valor: ")
+        a[f][c]=float(input())
+
+print("impresion de valores almacenados")
+for f in range(maxf):
+    for c in range(maxc):
+        print(str(a[f][c]), end=" ")
+    print()
+
